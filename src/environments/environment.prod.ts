55a909ed196439f22e41b7c8b7126d660a9f7ae9
Pame <pameac1501@gmail.com>
2020-12-26 20:56:54 -0300

    img

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBRzwhRIKARZThReGQcuAkfAOJQ_7NtBU8",
+    authDomain: "cordsupply.firebaseapp.com",
+    databaseURL: "https://cordsupply.firebaseio.com",
+    projectId: "cordsupply",
+    storageBucket: "cordsupply.appspot.com",
+    messagingSenderId: "197356020387",
+    appId: "1:197356020387:web:7a829b46f0823f711e9516"
+  }
 };
